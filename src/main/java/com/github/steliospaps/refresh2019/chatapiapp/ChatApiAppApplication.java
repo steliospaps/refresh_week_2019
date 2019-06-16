87ecf509e9b726b9174f283ea339b6e53b1a300a
stelios <stelios05@gmail.com>
2019-06-16 22:36:59 +0100

    initial

---
+++ src/main/java/com/github/steliospaps/refresh2019/chatapiapp/ChatApiAppApplication.java
@@ -0,0 +1,13 @@
+package com.github.steliospaps.refresh2019.chatapiapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatApiAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatApiAppApplication.class, args);
+	}
+
+}
