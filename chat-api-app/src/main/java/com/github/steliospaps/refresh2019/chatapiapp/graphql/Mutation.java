155228b40e0102f1cfc7f18dae3cd3a6023f5b72
stelios <stelios05@gmail.com>
2019-06-18 11:22:08 +0100

    USer create and list

--- chat-api-app/src/main/java/com/github/steliospaps/refresh2019/chatapiapp/graphql/Mutation.java
+++ chat-api-app/src/main/java/com/github/steliospaps/refresh2019/chatapiapp/graphql/Mutation.java
@@ -1,23 +1,33 @@
 package com.github.steliospaps.refresh2019.chatapiapp.graphql;
 
-import javax.transaction.Transactional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.coxautodev.graphql.tools.GraphQLMutationResolver;
 import com.github.steliospaps.refresh2019.chatapiapp.chat.db.Room;
 import com.github.steliospaps.refresh2019.chatapiapp.chat.db.RoomRepository;
+import com.github.steliospaps.refresh2019.chatapiapp.chat.db.User;
+import com.github.steliospaps.refresh2019.chatapiapp.chat.db.UserRepository;
 
 @Service
 public class Mutation implements GraphQLMutationResolver {
 
 	@Autowired
 	private RoomRepository roomRepository;
+
+	@Autowired
+	private UserRepository userRepository;
 	
 	public Room createRoom(String name) {
 		return roomRepository
 				.save(Room.builder()
+						.name(name)
+						.build());
+	}
+
+	public User createUser(String name) {
+		return userRepository
+				.save(User.builder()
 						.name(name)
 						.build());
 	}
--- chat-api-app/src/main/java/com/github/steliospaps/refresh2019/chatapiapp/graphql/Query.java
+++ chat-api-app/src/main/java/com/github/steliospaps/refresh2019/chatapiapp/graphql/Query.java
@@ -1,20 +1,28 @@
 package com.github.steliospaps.refresh2019.chatapiapp.graphql;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
 import com.github.steliospaps.refresh2019.chatapiapp.chat.db.Room;
 import com.github.steliospaps.refresh2019.chatapiapp.chat.db.RoomRepository;
-
-import lombok.AllArgsConstructor;
+import com.github.steliospaps.refresh2019.chatapiapp.chat.db.User;
+import com.github.steliospaps.refresh2019.chatapiapp.chat.db.UserRepository;
 
 @Service
-@AllArgsConstructor
 public class Query implements GraphQLQueryResolver{
 
-	private final RoomRepository roomRepository;
+	@Autowired
+	private RoomRepository roomRepository;
+	
+	@Autowired
+	private UserRepository userRepository;
 	
 	public Iterable<Room> getAllRooms() {
 		return roomRepository.findAll();
+	}	
+
+	public Iterable<User> getAllUsers() {
+		return userRepository.findAll();
 	}	
 }
